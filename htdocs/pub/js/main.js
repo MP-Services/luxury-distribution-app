105134ad34e90ac124764518b86490687bb120ea
datdt02 <datdt@avadagroup.com>
2024-04-09 15:22:22 +0700

    update answer helpdesk

--- htdocs/pub/js/main.js
+++ htdocs/pub/js/main.js
@@ -11,3 +11,17 @@ $(document).mouseup(function (e) {
         $('.content').removeClass('opacity')
     }
 });
+
+$(document).ready(function () {
+    $('button.action-btn').on('click', function() {
+        var answerItem = $(this).closest('.questions-answer').find('.answer-item')
+        answerItem.toggleClass('answer-active')
+        if(answerItem.hasClass('answer-active')) {
+            $(this).find('svg g path').remove()
+            $(this).find('svg g').html('<path d="M8 16H24M8 16L24 16" stroke="white" stroke-width="2" />')
+        } else {
+            $(this).find('svg g path').remove()
+            $(this).find('svg g').html('<path d="M23.2381 15.2381H16.7619V8.76191C16.7619 8.34134 16.4213 8 16 8C15.5787 8 15.2381 8.34134 15.2381 8.76191V15.2381H8.76191C8.34056 15.2381 8 15.5794 8 16C8 16.4206 8.34056 16.7619 8.76191 16.7619H15.2381V23.2381C15.2381 23.6587 15.5787 24 16 24C16.4213 24 16.7619 23.6587 16.7619 23.2381V16.7619H23.2381C23.6594 16.7619 24 16.4206 24 16C24 15.5794 23.6594 15.2381 23.2381 15.2381Z" fill="white"/>')
+        }
+    } )
+})
