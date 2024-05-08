40104fdc1a6d934f7bfb48d9081c5f8e687e51b3
quangdd <quangdd@avadagroup.com>
2024-05-08 14:32:08 +0700

    Fix Orders page html/css

--- packages/assets/src/pages/Orders/Orders.js
+++ packages/assets/src/pages/Orders/Orders.js
@@ -24,26 +24,24 @@ export default function Orders() {
         <div className="orders-detail-table">
           <div className="table-header">
             <span className="span-table-title">Data table</span>
-            <div className="items-per-page" style={{position: 'relative'}}>
-              <div className="select-wrapper">
-                <button className="select-btn-item">08 items per page</button>
-                <i className="fa-solid fa-chevron-down"></i>
-              </div>
-              <ul className="select-content">
-                <li className="number-of-page">10 items per page</li>
-                <li className="number-of-page">15 items per page</li>
-                <li className="number-of-page">20 items per page</li>
-              </ul>
+            <div className="items-per-page">
+              <select name="" id="" defaultValue={8} className="per-page-select">
+                <option value="1">01 items per page</option>
+                <option value="2">02 items per page</option>
+                <option value="3">03 items per page</option>
+                <option value="4">04 items per page</option>
+                <option value="8">08 items per page</option>
+              </select>
             </div>
           </div>
           <div className="orders-table-content">
             <table className="table" style={{width: '100%', textAlign: 'left', borderSpacing: 0}}>
               <thead>
                 <tr>
-                  <th className="col-nb-1">Retailer Order ID</th>
-                  <th className="col-nb-2">Dropshipper Order ID</th>
-                  <th className="order-status col-nb-3">Successful</th>
-                  <th className="col-nb-4">Created By</th>
+                  <th>Retailer Order ID</th>
+                  <th>Dropshipper Order ID</th>
+                  <th className="order-status">Successful</th>
+                  <th>Created By</th>
                 </tr>
               </thead>
               <tbody>
@@ -59,7 +57,7 @@ export default function Orders() {
                   <td data-th="Retail Order Id">JKAKDKKAA4454S</td>
                   <td data-th="Dropshipper Order Id">JKAKDKKAA4454S</td>
                   <td data-th="Successful" className="order-status">
-                    Successfull
+                    Successful
                   </td>
                   <td data-th="Created">Feb 13, 2024 10:16 AM</td>
                 </tr>
@@ -115,10 +113,10 @@ export default function Orders() {
             </table>
           </div>
           <div className="pagination-wrapper">
-            <div className="previous-btn">
+            <button className="paging-option">
               <i className="fa-solid fa-angle-left"></i>
-              <span className="pre-label">Previous</span>
-            </div>
+              <span>Previous</span>
+            </button>
             <div className="pagination-items">
               <div className="pagi-item pagi-active">1</div>
               <div className="pagi-item">2</div>
@@ -127,10 +125,10 @@ export default function Orders() {
               <div className="pagi-item">...</div>
               <div className="pagi-item">10</div>
             </div>
-            <div className="next-btn">
-              <span className="next-label">Next</span>
+            <button className="paging-option">
+              <span>Next</span>
               <i className="fa-solid fa-angle-right"></i>
-            </div>
+            </button>
           </div>
         </div>
       </div>
